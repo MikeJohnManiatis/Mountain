78be86e8e81333f1917541144dd36b33ee9d19a2
mike <mikejohnmaniatis@gmail.com>
2017-08-08 10:08:24 -0500

    comment

---
+++ Mountain/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+// using Mountain.Models;
+
+namespace Mountain.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            ViewBag.currentTime = DateTime.Now;
+            return View();
+        }
+
+        public ActionResult About()
+        { 
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
