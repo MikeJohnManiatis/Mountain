b4d64e680288d96e28cf276344b956a5c03ee2e2
MikeJohnManiatis <Mikejohnmaniatis@gmail.com>
2017-10-19 15:08:54 -0500

    New changes

--- Mountain/Controllers/AboutController.cs
+++ Mountain/Controllers/AboutController.cs
@@ -3,18 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-//using Mountain.Models;
 
 namespace Mountain.Controllers
 {
-    public class HomeController : Controller
+    public class AboutController : Controller
     {
+        // GET: About
         public ActionResult Index()
         {
-            ViewBag.currentTime = DateTime.Now.ToString("MM/dd/yyyy");
-         
             return View();
         }
-
     }
 }
