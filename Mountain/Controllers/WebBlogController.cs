a986328600af8e33f34c882415eb86098eb858f8
mike <mikejohnmaniatis@gmail.com>
2017-10-19 17:26:44 -0500

    about me changes/blog button

---
+++ Mountain/Controllers/WebBlogController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mountain.Controllers
+{
+    public class WebBlogController : Controller
+    {
+        // GET: WebBlog
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
